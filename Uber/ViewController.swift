26ce88411a8ad60914b4287c0f947759cefb2b59
emil vaklinov <emil_vaklinov@yahoo.com>
2020-05-05 13:29:54 +0100

    view controller connected

--- Uber/ViewController.swift
+++ Uber/ViewController.swift
@@ -10,6 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var riderDriverSwitch: UISwitch!
+    @IBOutlet weak var bottomButton: UIButton!
+    @IBOutlet weak var topButton: UIButton!
+    
+    
+    @IBAction func topTapped(_ sender: Any) {
+    }
+    
+    @IBAction func bottomTapped(_ sender: Any) {
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
