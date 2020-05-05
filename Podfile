e96917f2e6f00dd06ec57b011221aa3e4ae4fe24
emil vaklinov <emil_vaklinov@yahoo.com>
2020-05-05 13:43:55 +0100

    bottom button created with hiding labels and switch

--- Podfile
+++ Podfile
@@ -7,6 +7,7 @@ target 'Uber' do
 
   pod 'Firebase/Analytics'
   pod 'Firebase/Core'
+  pod 'Firebase/Auth'
 
 end
 
--- Uber/ViewController.swift
+++ Uber/ViewController.swift
@@ -7,26 +7,47 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var riderLabel: UILabel!
+    @IBOutlet weak var driverLabel: UILabel!
     @IBOutlet weak var emailTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
     @IBOutlet weak var riderDriverSwitch: UISwitch!
     @IBOutlet weak var bottomButton: UIButton!
     @IBOutlet weak var topButton: UIButton!
     
+    var signUpMode = true
     
-    @IBAction func topTapped(_ sender: Any) {
-    }
-    
-    @IBAction func bottomTapped(_ sender: Any) {
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    @IBAction func topTapped(_ sender: Any) {
+        
+    }
+    
+    @IBAction func bottomTapped(_ sender: Any) {
+        if signUpMode {
+            topButton.setTitle("Log In", for: .normal)
+            bottomButton.setTitle("Switch to Sign Up", for: .normal)
+            riderLabel.isHidden = true
+            driverLabel.isHidden = true
+            riderDriverSwitch.isHidden = true
+            signUpMode = false
+        } else {
+            topButton.setTitle("Sign Up", for: .normal)
+            bottomButton.setTitle("Switch to Log In", for: .normal)
+            riderLabel.isHidden = false
+            driverLabel.isHidden = false
+            riderDriverSwitch.isHidden = false
+            signUpMode = true
+        }
+    }
+    
+    
 }
 
