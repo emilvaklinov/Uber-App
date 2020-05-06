0d38ea2d72ca8f8db7235e393fcaa48b721e5a2a
emil vaklinov <emil_vaklinov@yahoo.com>
2020-05-06 15:11:33 +0100

    display driver and rider function added

--- Uber/Controllers/RiderViewController.swift
+++ Uber/Controllers/RiderViewController.swift
@@ -19,6 +19,8 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate {
     var locationManager = CLLocationManager()
     var userLocation = CLLocationCoordinate2D()
     var uberHasBeenCalled = false
+    var driverOnTheWay = false
+    var driverLocation = CLLocationCoordinate2D()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,6 +36,31 @@ class RiderViewController: UIViewController, CLLocationManagerDelegate {
                 Database.database().reference().child("RideRequest").removeAllObservers()
             })
         }
+    }
+    
+    func displayDriverAndRider() {
+        let driverCLLocation = CLLocation(latitude: driverLocation.latitude, longitude: driverLocation.longitude)
+        let riderCLLocation = CLLocation(latitude: userLocation.latitude, longitude: userLocation.longitude)
+        let distance = driverCLLocation.distance(from: riderCLLocation) / 1000
+        let roundedDistance = round(distance * 100) / 100
+        callAnUberButton.setTitle("Your driver is \(roundedDistance)km away!", for: .normal)
+        uberMap.removeAnnotations(uberMap.annotations)
+        
+        let latDelta = abs(driverLocation.latitude - userLocation.latitude) * 2 + 0.005
+        let lonDelta = abs(driverLocation.longitude - userLocation.longitude) * 2 + 0.005
+
+        let region = MKCoordinateRegion(center: userLocation, span: MKCoordinateSpan(latitudeDelta: latDelta, longitudeDelta: lonDelta))
+        uberMap.setRegion(region, animated: true)
+
+        let riderAnno = MKPointAnnotation()
+        riderAnno.coordinate = userLocation
+        riderAnno.title = "Your Location"
+        uberMap.addAnnotation(riderAnno)
+
+        let driverAnno = MKPointAnnotation()
+        driverAnno.coordinate = driverLocation
+        driverAnno.title = "Your Driver"
+        uberMap.addAnnotation(driverAnno)
     }
     // Location update
        func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
